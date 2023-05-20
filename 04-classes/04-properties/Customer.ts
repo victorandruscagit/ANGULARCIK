1552953478cf547e92d190ce199c30a58f7dd6a5
victorandruscagit <victor.v.andrusca@gmail.com>
2023-05-20 14:24:48 +0300

    properties with constructor magic

---
+++ 04-classes/04-properties/Customer.ts
@@ -0,0 +1,36 @@
+class Customer{
+    
+  
+
+
+    
+constructor(private  _firstName :string,  private _lastName: string){
+  
+  
+}
+
+
+public get firstName(): string {
+    return this._firstName;
+}
+public set firstName(value: string ) {
+    this._firstName = value;
+
+}
+
+public get lastName(): string {
+    return this._lastName;
+}
+public set lastName(value: string ) {
+    this._lastName = value;
+
+}
+
+}
+
+
+
+let customer = new Customer("calin", "goia");
+
+console.log(customer.firstName);
+console.log(customer.lastName);
