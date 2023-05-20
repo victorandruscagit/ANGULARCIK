ed0b029dcc0c9aad7249c147cce620297c5ffa77
victorandruscagit <victor.v.andrusca@gmail.com>
2023-05-20 14:31:35 +0300

    module import/export?

---
+++ 04-classes/05-modules/Driver.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Customer_1 = require("./Customer");
+var customer = new Customer_1.Customer("calin", "goia");
+console.log(customer.firstName);
+console.log(customer.lastName);
---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,6 @@
+
+import { Customer } from "./Customer";
+let customer = new Customer("calin", "goia");
+
+console.log(customer.firstName);
+console.log(customer.lastName);
